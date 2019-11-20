bb85f1dfd6d8cf1e2c9f2e3b4e6d80f9125a8e6a
LubaKaper <liubovkaper@pursuit.com>
2019-11-20 16:38:47 -0500

    created table view

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = [Movie]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadData()
+        tableView.dataSource = self
     }
-
+    func loadData() {
+        movies = Movie.allMovies
+    }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
+        let movie = movies[indexPath.row]
+        
+        cell.textLabel?.text = movie.name
+        cell.detailTextLabel?.text = movie.year.description
+        return cell 
+    }
+}
